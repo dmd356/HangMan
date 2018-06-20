573cda3d12844c5c4dff58764501c1fc63627c6b
dmd356 <dmd356@gmail.com>
2018-06-20 13:04:52 -0700

    Add files via upload

---
+++ Week_4_Course_Project/Week_4_Course_Project/Menu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace Week_4_Course_Project
+{
+    public class Menu
+    {
+
+
+        public void introStage(){
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Hello, Welcome to Hangman!");
+            Console.WriteLine("please pick a mode!");
+        }
+
+      
+
+        public void creditStage()
+        {
+            
+            Console.WriteLine("");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Thank you for playing Hangman!");
+            Console.WriteLine("Game Designed by: Devin Distelhorst");
+            Console.WriteLine("Script Designed by: Devin Distelhorst!");
+            Console.WriteLine("Animations Designed by: Devin Distelhorst!");
+            Console.WriteLine("Orchestra/Opera Music Designed by: Devin Distelhorst!");
+            Console.WriteLine(" Designed by: Devin Distelhorst!");
+
+
+
+
+        }
+        public int PickInt(String say, int maxOptions)
+        {
+            while (true)
+            {
+                Console.WriteLine(say);
+                String answer = Console.ReadLine();
+                int num;
+                if (int.TryParse(answer, out num) && num<=maxOptions)
+                {
+                    return num;
+                }
+            }
+        }
+
+        public String pickLetter()
+        {
+            while (true)
+            {
+                String answer = Console.ReadLine();
+                if (answer.Length < 2 && answer.Length > 0)
+                {
+                    return answer;
+                }
+                Console.WriteLine("Please guess one alphabetical letter only.");
+            }
+        }//pick letter end
+    }
+}
---
+++ Week_4_Course_Project/Week_4_Course_Project/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Week_4_Course_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] words = { "h e l l o", "1 2 3 4 5", "c a l i f o r n i a"};
+            bool gameOn = true;
+            while (gameOn)//if game is still playing
+            {
+                Menu menu = new Menu();
+                int lives = 5;
+                int score = 0;
+                String say = "";
+                //Game Starts
+                menu.introStage();
+                say = "0 for easy, 1 for medium, 2 for hard ";
+                Console.WriteLine("--------------------------");
+
+                int mode = menu.PickInt(say, 2);
+                //Initialize the game words
+                String[] truth = words[mode].Split(" ");
+                String[] guesses = new String[truth.Length];
+                String trueAnswer = "";
+                Console.WriteLine("The word goes like ");
+                for (int i = 0; i < truth.Length; i++)//print the word
+                {
+                    guesses[i] = "x";
+                    Console.Write(guesses[i]);
+                    trueAnswer += truth[i];
+                }
+                Console.WriteLine();
+
+                while (lives > 0 && score < truth.Length)//game loop 2!
+                {
+                    score = 0;
+                    Console.WriteLine("Please guess a letter");
+                    String answer = menu.pickLetter();
+                    int fail = 0;
+                    for (int j = 0; j < truth.Length; j++)//Checking their answer if its true
+                    {
+                        if (truth[j] == answer.ToLower())
+                        {
+                            guesses[j] = guesses[j].Replace(guesses[j].Substring(0), answer);
+                        }
+                        else
+                        {
+                            fail++;
+                        }
+                    }//for j
+
+                    foreach (String x in guesses) //read the guessed list
+                    {
+                        Console.Write(x);
+                    }
+
+                    Console.WriteLine();
+
+                    for (int i = 0; i < truth.Length; i++)
+                    {
+                        if (truth[i].ToLower() == guesses[i].ToLower())
+                        {
+                            score++;
+                        }
+                    }
+                    Console.WriteLine("Score : " + score + " out of " + truth.Length);
+                    if (fail == truth.Length)
+                    {
+                        lives--;
+                        Console.WriteLine("Guess again? You're lives are at " + lives);
+                    }
+                    Console.WriteLine();
+                }//while
+
+
+                Console.WriteLine("The word was " + trueAnswer+"!");
+                if (lives == 0)
+                {
+                    Console.WriteLine("You lose!");
+                }
+                else
+                {
+                    Console.WriteLine("You win!");
+                }
+                Console.WriteLine();
+                say = "Continue? 0 for no, 1 for yes";
+                gameOn = (menu.PickInt(say, 1) > 0) ? true : false;
+                Console.WriteLine();
+            }//End game loop
+            Console.WriteLine("Goodbye!!!");
+        }
+    }
+}
